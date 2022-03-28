61cf765f4a047a6ee1c9711491f7c80ddf5a2e42
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-03-28 11:51:26 +0200

    edit UI skeleton

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -8,6 +8,7 @@ import {
   Navbar,
   NavbarBrand,
   Row,
+  Stack,
   Tabs,
   ToggleButton,
   ToggleButtonGroup,
@@ -17,13 +18,29 @@ export const Main = () => {
   const [key, setKey] = useState("hourly");
   const [value, setValue] = useState("f");
   const handleChange = (val) => setValue(val);
-
   const date = new Date();
 
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    // status.textContent = '';
+    // mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    // mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    // status.textContent = 'Unable to retrieve your location';
+  }
+
+  if (!navigator.geolocation) {
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
   return (
-    <Container fluid className="text-light">
-      <Navbar className="text-light">
-        <NavbarBrand>INSTAWEATHER</NavbarBrand>
+    <Container className="text-light">
+      <Navbar>
+        <NavbarBrand className="text-light me-auto">INSTAWEATHER</NavbarBrand>
         {/* to add toggle button */}
         <ToggleButtonGroup
           type="checkbox"
@@ -39,23 +56,26 @@ export const Main = () => {
         </ToggleButtonGroup>
       </Navbar>
       {/* Main container */}
-      <Container>
+      <Container className="p-5">
         <Row>
           {/* Location */}
           <Col xs="6">
             <Row>
-              <Col xs="12">New Cairo</Col>
+              <Col xs="12" className="fs-3 ">
+                New Cairo
+              </Col>
               <Col>{`${date.getDate()} ,${date.getMonth()}, ${date.getFullYear()}`}</Col>
               <Col xs="12"></Col>
               <Col xs="12">
                 <img src={cloudy} />
               </Col>
+              <Col xs="12">Cloudy</Col>
             </Row>
           </Col>
           {/* Weather */}
           <Col xs="6">
-            <Col>72 deg</Col>
-            <Col>81/83</Col>
+            <Col className="fs-3">72 °</Col>
+            <Col>81° /83° </Col>
             <Col>cloud through</Col>
           </Col>
         </Row>
@@ -71,7 +91,68 @@ export const Main = () => {
           <Tab eventKey="hourly" title="Hourly"></Tab>
           <Tab eventKey="daily" title="Daily"></Tab>
         </Tabs>
-        <Container fluid>{<>Tables</>}</Container>
+        <Container fluid>
+          {
+            <Row>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              <Stack gap={3} style={{ width: "3rem" }} className="d-flex align-items-center">
+                <div>10:00 </div>
+                <img src={cloudy} style={{ width: "3rem" }} className=""/>
+                <div>81 deg</div>
+              </Stack>
+              
+            </Row>
+          }
+        </Container>
       </Container>
     </Container>
   );
